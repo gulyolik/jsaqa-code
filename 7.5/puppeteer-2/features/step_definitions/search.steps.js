ca3e122af7a82d926fc4515bc339707e5efad482
gulyolik <olia.guliaeva@yandex.ru>
2024-02-19 18:58:06 +0300

    cucumber HW

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -17,18 +17,50 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+Given("user is on poster page", async function () {
+  return await this.page.goto("http://qamid.tmweb.ru/client/", {
+    setTimeout: 60 * 1000,
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("user selects available session", async function () {
+  return await clickElement(
+    this.page,
+    ".movie-seances__time:not(.acceptin-button-disabled)"
+  );
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+When("user reserves one free seat", async function () {
+  await clickElement(
+    this.page,
+    ".buying-scheme__wrapper .buying-scheme__chair:not(.buying-scheme__chair_taken, .buying-scheme__chair_selected)"
+  );
+});
+
+When("user reserves free VIP seat", async function () {
+  await clickElement(
+    this.page,
+    ".buying-scheme__wrapper .buying-scheme__chair:not(.buying-scheme__chair .buying-scheme__chair_vip)"
+  );
+});
+
+When("user selects the occupied seat", async function () {
+  return await clickElement(
+    this.page,
+    ".buying-scheme__wrapper .buying-scheme__chair_taken"
+  );
+});
+
+Then ("user sees that his place is chosen", async function (string) {
+  expect(this.page).contain(".buying-scheme__chair_selected");
+})
+
+Then("button {string} is disabled", async function (string) {
+  const actualAttribtue = await this.page.$eval(
+    "button.acceptin-button",
+    (link) => link.getAttribute("disabled")
+  );
+  const actualText = await getText(this.page, "button.acceptin-button");
+  expect(actualAttribtue).contain("true");
+  expect(actualText).contain(string);
 });
